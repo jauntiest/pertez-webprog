c6cd7286ed5f5514f249725b74d74ee12ba44a77
JDev <jdrpertez@gmail.com>
2026-08-03 19:10:25 +0800

    lab-act3

---
+++ pertez-client/src/pages/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFoundPage() {
+    return (
+        <>
+        
+        <h1>Page Not Found</h1>
+        <p>The link you followed to get here must be broken...</p>
+        </>
+    )
+}
+
+export default NotFoundPage;
